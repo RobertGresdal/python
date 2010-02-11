ca1a8434fd16100509c6ab0c197e562ff767fa06
Robert Gresdal <robert@gresdal.no>
2010-02-11 14:30:31 +0100

    Check to see if input is executed

---
+++ basics/read_input.py
@@ -0,0 +1,6 @@
+foo = input("Try to type eval(2+2), if the result is '4' this is unsafe: ")
+print("You typed the following: \"",foo,"\".")
+input("Press [Enter] to exit")
+# Result: seems to be safe with Python 3.1
+# Found original tip here: http://www.daniweb.com/forums/thread12326.html
+# It's from back in 2004, might have been unsafe then
